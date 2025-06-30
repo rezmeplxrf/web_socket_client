64e05b1dd7c2fba961c1409735827753cdbcbebe
Donghu <rezmeplx@rfway.org>
2025-06-30 13:41:00 +0900

    fix: include NoBackoff in backoff exports from web_socket_client.dart

--- lib/web_socket_client.dart
+++ lib/web_socket_client.dart
@@ -1,6 +1,6 @@
 export 'package:web_socket_channel/web_socket_channel.dart';
 
-export 'src/backoff/backoff.dart' show Backoff;
+export 'src/backoff/backoff.dart' show Backoff, NoBackoff;
 export 'src/backoff/binary_exponential_backoff.dart'
     show BinaryExponentialBackoff;
 export 'src/backoff/constant_backoff.dart' show ConstantBackoff;

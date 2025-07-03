b3c48def40396959f97c71419195e66986dc3522
Donghu <rezmeplx@rfway.org>
2025-07-03 20:04:09 +0900

    fix: remove distinct filter from WebSocket message stream listener

--- lib/src/web_socket.dart
+++ lib/src/web_socket.dart
@@ -113,7 +113,7 @@ class WebSocket {
       ).timeout(_timeout);
       _channel = getWebSocketChannel(ws);
 
-      _subscription = _channel?.stream.distinct().listen(
+      _subscription = _channel?.stream.listen(
           (msg) {
             if (msg is String) {
               _onMessage(msg);
